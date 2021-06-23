35dec43eb54a952f51afe0f7642f88bc3bf6aabc
maggicco <maggicco83@hotmail.fr>
2021-06-23 17:30:10 +0200

    approach to target getData Intent

--- app/src/main/java/com/openclassrooms/entrevoisins/ui/neighbour_list/MyNeighbourRecyclerViewAdapter.java
+++ app/src/main/java/com/openclassrooms/entrevoisins/ui/neighbour_list/MyNeighbourRecyclerViewAdapter.java
@@ -4,13 +4,16 @@ import android.content.Context;
 import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.request.RequestOptions;
@@ -26,10 +29,14 @@ import java.util.List;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
+import static org.greenrobot.eventbus.EventBus.TAG;
+
 public class MyNeighbourRecyclerViewAdapter extends RecyclerView.Adapter<MyNeighbourRecyclerViewAdapter.ViewHolder> {
 
     private final List<Neighbour> mNeighbours;
+    private Context context;
     ItemClicked activity;
+    private ListView listView;
     TextView name;
     //private boolean favoritesButton;
 
@@ -46,23 +53,6 @@ public class MyNeighbourRecyclerViewAdapter extends RecyclerView.Adapter<MyNeigh
 
     }
 
-    public class ViewHolder extends RecyclerView.ViewHolder {
-        @BindView(R.id.item_list_avatar)
-        public ImageView mNeighbourAvatar;
-        @BindView(R.id.item_list_name)
-        public TextView mNeighbourName;
-        @BindView(R.id.item_list_delete_button)
-        public ImageButton mDeleteButton;
-
-        TextView neighbourName;
-
-
-        public ViewHolder(View view) {
-            super(view);
-            neighbourName = view.findViewById(R.id.textViewNeighbourName);
-            ButterKnife.bind(this, view);
-        }
-    }
 
     @NonNull
     @Override
@@ -77,18 +67,24 @@ public class MyNeighbourRecyclerViewAdapter extends RecyclerView.Adapter<MyNeigh
         Neighbour neighbour = mNeighbours.get(position);
         holder.mNeighbourName.setText(neighbour.getName());
 
-        Glide.with(holder.mNeighbourAvatar.getContext())
-                .load(neighbour.getAvatarUrl())
+        Glide.with(holder.mNeighbourAvatar.getContext()).load(neighbour.getAvatarUrl())
                 .apply(RequestOptions.circleCropTransform())
                 .into(holder.mNeighbourAvatar);
 
 
-        holder.itemView.setOnClickListener(v -> {
 
-            Intent intent = new Intent(v.getContext()
-                    , NeighbourProfileDetailsActivity.class);
-
-            v.getContext().startActivity(intent);
+        holder.itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(v.getContext()
+                        , NeighbourProfileDetailsActivity.class);
+                String name = mNeighbours.get(position).getName();
+                Log.d(TAG, "onClick: "+ mNeighbours.get(position).getName());
+                String adress = mNeighbours.get(position).getAddress();
+                //Toast.makeText(context, (CharSequence) mNeighbours.get(position), Toast.LENGTH_SHORT).show();
+                //intent.putExtra("neighbour_name", mNeighbours.get(position).getName());
+                v.getContext().startActivity(intent);
+            }
         });
 
         holder.mDeleteButton.setOnClickListener(new View.OnClickListener() {
@@ -97,6 +93,25 @@ public class MyNeighbourRecyclerViewAdapter extends RecyclerView.Adapter<MyNeigh
                 EventBus.getDefault().post(new DeleteNeighbourEvent(neighbour));
             }
         });
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        @BindView(R.id.item_list_avatar)
+        public ImageView mNeighbourAvatar;
+        @BindView(R.id.item_list_name)
+        public TextView mNeighbourName;
+        @BindView(R.id.item_list_delete_button)
+        public ImageButton mDeleteButton;
+
+        TextView neighbourName;
+
+
+        public ViewHolder(View view) {
+            super(view);
+            neighbourName = view.findViewById(R.id.textViewNeighbourName);
+
+            ButterKnife.bind(this, view);
+        }
     }
 
     @Override
